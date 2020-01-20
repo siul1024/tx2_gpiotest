1685a0e3b1fd557622a67880b06d0ff8f89444bf
junghwan Lee <kar00034@naver.com>
2020-01-20 17:42:55 +0900

    이름 변경

--- tx2_gpiotest/speed_control/m_speed_control.py
+++ tx2_gpiotest/speed_control/m_speed_control.py
@@ -1,9 +1,9 @@
 from PyQt5 import QtWidgets
 
-from MainWindow_ui import Main_Control
+from tx2_gpiotest.speed_control.speed_control_ui import Speed_Control
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    w = Main_Control()
+    w = Speed_Control()
     app.exec_()
 
--- tx2_gpiotest/speed_control/speed_control_ui.py
+++ tx2_gpiotest/speed_control/speed_control_ui.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 # 한글표시
 from PyQt5 import uic, QtWidgets
-import PyQt5.QtWidgets
+# from adafruit_servokit import ServoKit
 
-class Main_Control(QtWidgets.QWidget):
+class Speed_Control(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
-        self.ui = uic.loadUi("MainWindow.ui")
+        self.ui = uic.loadUi("speed_control.ui")
         self.ui.show()
 
-        self.v_speed = 0    # 회전 방향 default
+        self.v_speed = 90    # 회전 방향 default
         self.l_speed = 0    # 직진 속도 default
+        # self.kit = ServoKit(channels=8)
+
+        self.ui.btn_a.setCheckable(True)
+
 
         self.ui.btn_w.clicked.connect(self.sp_u)    # alt + w 입력시 직진속도 +
         self.ui.btn_s.clicked.connect(self.sp_d)    # alt + s 입력시 직진속도 -
-        self.ui.btn_a.clicked.connect(self.sp_l)    # alt + a 입력시 회전 방향 -
+        self.ui.btn_a.pressed.connect(self.sp_l)    # alt + a 입력시 회전 방향 -
         self.ui.btn_d.clicked.connect(self.sp_r)    # alt + d 입력시 회전 방향 +
         self.ui.btn_stop.clicked.connect(self.sp_s) # alt + f 입력시 속도 0
 
@@ -28,16 +32,23 @@ class Main_Control(QtWidgets.QWidget):
 
     def sp_l(self):
         self.v_speed = self.v_speed - 10
+
+        if self.v_speed <0:
+            self.v_speed=0
+
         self.Status()
 
     def sp_r(self):
         self.v_speed = self.v_speed + 10
-        self.Status()
 
+        if self.v_speed >180:
+            self.v_speed=180
+
+        self.Status()
 
     def sp_s(self):
         self.l_speed = 0
-        self.v_speed = 0
+        self.v_speed = 90
         self.Status()
 
     def Status(self):   # 상태 표시
@@ -48,9 +59,11 @@ class Main_Control(QtWidgets.QWidget):
         else:
             self.ui.lbl_show_ls.setText(str(self.l_speed) + ' 후진')
 
-        if (self.v_speed < 0):  # 회전 방향 및 수치 표시
+        if (self.v_speed < 90):  # 회전 방향 및 수치 표시
             self.ui.lbl_show_vs.setText(str(self.v_speed)+' 좌회전')
-        elif(self.v_speed == 0):
+        elif(self.v_speed == 90):
             self.ui.lbl_show_vs.setText(str(self.v_speed)+' 직진')
         else:
             self.ui.lbl_show_vs.setText(str(self.v_speed)+' 우회전')
+
+        # self.kit.servo[2].angle = self.v_speed
